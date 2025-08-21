1ea6eb0e58b7294b8ea0693a006ed5d1112be3f8
Malaiarasu G <govindarajmalaiarasu@gmail.com>
2025-08-21 09:58:44 +0000

    makle blue for the actice tab, and blue in hover in start over button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background hover:bg-primary hover:text-primary-foreground",
         "outline-primary":
           "border border-primary bg-transparent text-primary hover:bg-primary hover:text-primary-foreground",
         secondary:
