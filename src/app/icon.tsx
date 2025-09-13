da928a968e6742220f027937de435e17407c51be
Malaiarasu G <govindarajmalaiarasu@gmail.com>
2025-09-13 08:36:25 +0000

    i see only black circle in the tab

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,5 +1,4 @@
 import { ImageResponse } from 'next/og'
-import { Logo } from '@/components/icons';
 
 export const runtime = 'edge'
 export const size = {
@@ -23,7 +22,23 @@ export default function Icon() {
           borderRadius: '50%',
         }}
       >
-        <Logo color="white" size={20}/>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="white"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="m16 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z"/>
+          <path d="m2 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z"/>
+          <path d="M7 21h10"/>
+          <path d="M12 3v18"/>
+          <path d="M3 7h18"/>
+        </svg>
       </div>
     ),
     {
